3aabd7fa8b28c6b87112a41f9c68f97e60855f10
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2024-07-31 06:40:07 +0800

    backup

--- web/js/css_attr.js
+++ web/js/css_attr.js
@@ -1,8 +1,8 @@
 /**
- * @description CSS attr()方法的支持和使用
+ * @description CSS attr()方法的支援和使用
  * @author zhangxinxu(.com) 2020-08-11
  * @docs https://www.zhangxinxu.com/wordpress/?p=9443
- * @license MIT 作者和出处保留
+ * @license MIT 作者和出處保留
  */
 
 (function () {
@@ -19,10 +19,10 @@
         NodeList.prototype.forEach = Array.prototype.forEach;
     }
 
-    // 观察的元素选择器
+    // 觀察的元素選擇器
     var watchSelector = window.watchSelector || '*';
 
-    // 获取页面中所有的CSS自定义属性
+    // 取得頁面中所有的CSS自定義屬性
     var isSameDomain = function (styleSheet) {
         if (!styleSheet.href) {
             return true;
@@ -52,26 +52,26 @@
         }, []);
     })();
 
-    // 使用了keyword()语法的CSS自定义属性名
+    // 使用了keyword()語法的CSS自定義屬性名
     var arrCssPropsValueIsAttr = arrCSSCustomProps.filter(function (arrPropVal) {
         return /attr\([\w\W]+\)/i.test(arrPropVal[1]);
     });
 
-    // attr()语法的解析
-    // 返回对应的<attr-name> <type-or-unit> 和 <attr-fallback>
+    // attr()語法的解析
+    // 回傳對應的<attr-name> <type-or-unit> 和 <attr-fallback>
     var funParseAttr = function (valueVar) {
         var attrName, typeOrUnit, attrFallback;
         if (valueVar) {
 
             valueVar = valueVar.replace(/attr\(([\w\W]*)\)/i, '$1');
-            // fallback获取
+            // fallback取得
             var arrValueVar = valueVar.split(',');
-            // 这是后备样式，如果没有对应的属性，则使用这个值
+            // 這是備用樣式，如果沒有對應的屬性，則使用這個值
             if (arrValueVar.length > 1) {
                 attrFallback = arrValueVar[1].trim();
             }
 
-            // 前面的属性和单位
+            // 前面的屬性和單位
             var arrFirstPart = arrValueVar[0].trim().split(/\s+/);
             attrName = arrFirstPart[0];
             typeOrUnit = arrFirstPart[1] || 'string';
@@ -84,11 +84,11 @@
         };
     };
 
-    // attr()语法转换成目前CSS变量可识别的语法
+    // attr()語法轉換成目前CSS變數可識別的语法
     var funAttrVar2NormalVar = function (objParseAttr, valueAttr) {
-        // attr()语法 attr( <attr-name> <type-or-unit>? [, <attr-fallback> ]? )
-        // valueVar示意：attr(bgcolor color, deeppink)
-        // valueAttr示意： 'deepskyblue'或者null
+        // attr()語法 attr( <attr-name> <type-or-unit>? [, <attr-fallback> ]? )
+        // valueVar範例：attr(bgcolor color, deeppink)
+        // valueAttr範例： 'deepskyblue'或者null
 
         var attrName = objParseAttr.attrName;
         var typeOrUnit = objParseAttr.typeOrUnit;
@@ -99,7 +99,7 @@
         var arrUnits = ['ch', 'em', 'ex', 'ic', 'lh', 'rlh', 'rem', 'vb', 'vi', 'vw', 'vh', 'vmin', 'vmax', 'mm', 'cm', 'in', 'pt', 'pc', 'px', 'deg', 'grad', 'rad', 'turn', 'ms', 's', 'Hz', 'kHz', '%'];
 
         var valueVarNormal = valueAttr;
-        // 如果是string类型
+        // 如果是string類型
         switch (typeOrUnit) {
             case 'string': {
                 valueVarNormal = '"' + valueAttr + '"';
@@ -113,7 +113,7 @@
             }
         }
 
-        // 数值变单位的处理
+        // 數值變單位的處理
         if (arrUnits.includes(typeOrUnit) && valueAttr.indexOf(typeOrUnit) == -1 && parseFloat(valueAttr) == valueAttr) {
             valueVarNormal = parseFloat(valueAttr) + typeOrUnit;
         }
@@ -121,80 +121,80 @@
         return valueVarNormal;
     };
 
-    // 设置自定义属性值的方法
+    // 設定自定義屬性值的方法
     var funSetAttr = function (node) {
         if (node.nodeType != 1 || node.matches(watchSelector) == false) {
             return;
         }
 
-        // 通配符匹配时候有些元素忽略
+        // 通配符匹配時候有些元素忽略
         if (watchSelector == '*' && ['script', 'style', 'meta', 'title', 'head'].includes(node.nodeName.toLowerCase())) {
             return;
         }
 
         var objStyle = window.getComputedStyle(node);
 
-        // 当前节点的所有样式对象
+        // 當前節點的所有樣式對象
         var objStyle = window.getComputedStyle(node);
 
-        // 所有设置了keyword()的自定义属性的遍历处理
+        // 所有設定了keyword()的自定義屬性的遍歷處理
         arrCssPropsValueIsAttr.forEach(function (arr) {
             var cssProp = arr[0];
             var cssValue = node['originCssValue' + cssProp] || arr[1];
 
-            // 判断当前节点有没有设置对应的自定义属性
+            // 判斷當前節點有沒有設定對應的自定義屬性
             var cssVarValueAttr = objStyle.getPropertyValue(cssProp);
 
             if (!cssVarValueAttr || !cssVarValueAttr.trim() || (!/attr\(([\w\W]*)\)/i.test(cssVarValueAttr) && !node['originCssValue' + cssProp])) {
                 return;
             }
 
-            // 这个是HTML属性改变时候用的
+            // 這個是HTML屬性改變時用的
             if (!node['originCssValue' + cssProp]) {
                 node['originCssValue' + cssProp] = cssValue;
             }            
 
-            // 总是使用初始获取的自定义属性值
+            // 總是使用初始取得的自定義屬性值
             cssVarValueAttr = cssValue;
 
             var objParseAttr = funParseAttr(cssVarValueAttr);
 
-            // 获取属性对应的值
+            // 取得屬性對應的值
             if (!objParseAttr.attrName) {
                 return;
             }
 
-            // attr()属性名
+            // attr()屬性名
             var attrName = objParseAttr.attrName;
 
-            // 获取此时节点这些属性目前对应的值
-            // 如果没有值，则使用后备值
+            // 取得此時節點這些屬性目前對應的值
+            // 如果沒有值，則使用備用值
             var strHtmlAttr = node.getAttribute(attrName) || objParseAttr.attrFallback;
             if (!strHtmlAttr) {
-                // 设置为空
+                // 設定為空
                 node.style.setProperty(cssProp, '');
                 return;
             }
 
-            // 标记需要观察的HTML属性
+            // 標示需要觀察的HTML屬性
             node.attrNeedWatch = node.attrNeedWatch || [];
             if (node.attrNeedWatch.includes(attrName) == false) {
                 node.attrNeedWatch.push(attrName);
             }
 
             // 核心方法
-            // 浏览器不支持的attr()语法转变成支持的语法
+            // 瀏覽器不支持的attr()語法轉變成支持的語法
             var valueVarNormal = funAttrVar2NormalVar(objParseAttr, strHtmlAttr);
 
             console.log(valueVarNormal);
-            // 设置
+            // 設定
             node.style.setProperty(cssProp, valueVarNormal);
         });
     };
 
 
     var funAutoInitAndWatching = function () {
-        // DOM Insert自动初始化
+        // DOM Insert自動初始化
         if (window.MutationObserver) {
             var observerSelect = new MutationObserver(function (mutationsList) {
                 mutationsList.forEach(function (mutation) {
@@ -203,7 +203,7 @@
                     nodeAdded.forEach(function (eleAdd) {
                         funSetAttr(eleAdd);
                     });
-                    // 如果是属性发生变化
+                    // 如果是屬性發生變化
                     var attributeName = mutation.attributeName;
 
                     if (mutation.target && mutation.target.attrNeedWatch && mutation.target.attrNeedWatch.includes(attributeName)) {
@@ -219,7 +219,7 @@
             });
         }
 
-        // 如果没有开启自动初始化，则返回
+        // 如果沒有開啟自動初始化，則返回
         document.querySelectorAll(watchSelector).forEach(function (ele) {
             funSetAttr(ele);
         });
