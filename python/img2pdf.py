2a35ef19680092adce88b5a1d57babb5e7e3633f
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2024-09-14 06:31:37 +0800

    Update img2pdf.py

--- python/img2pdf.py
+++ python/img2pdf.py
@@ -1,6 +1,7 @@
 import img2pdf
 from PIL import Image
 
+import os
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -15,6 +16,7 @@ img_path = "C:/Users/Admin/Desktop/GfG_images/do_nawab.png" # storing image path
 pdf_path = "C:/Users/Admin/Desktop/GfG_images/file.pdf" # storing pdf path
 image = Image.open(img_path) # opening image
 pdf_bytes = img2pdf.convert(image.filename) # converting into chunks using img2pdf
+'''only_file_name = image.filename.split(os.path.sep)[-1]'''
 file = open(pdf_path, "wb") # opening or creating pdf file
 file.write(pdf_bytes) # writing pdf files with chunks
 image.close() # closing image file
