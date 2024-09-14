020469032a27d72b15193b4b2a396570e23ec984
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2024-09-15 06:12:59 +0800

    Update img2pdf.py

--- python/img2pdf.py
+++ python/img2pdf.py
@@ -11,49 +11,58 @@ file_types = (('image files', ('*.jpg', '*.jpeg', '*.jpe', '*.jfif', '*.png', '*
 
 window = tk.Tk()
 window.title = 'image to PDF file converter'
-window.geometry('300x200')
+window.geometry('400x200')
 window.resizable(False, False)
 window.iconbitmap(os.path.join('.', 'img', 'favicon.ico'))
 
 title_label = ttk.Label(text='image to PDF file converter')
 title_label.grid(column=0, row=0, columnspan=2)
 
-img_path = tk.StringVar(window, '')
-img_path_entry = ttk.Entry(window, textvariable=img_path)
+img_path_entry_stringvar = tk.StringVar(window, '')
+img_path_entry = ttk.Entry(window, textvariable=img_path_entry_stringvar, width=40)
 img_path_entry.grid(column=0, row=1)
-def reconfirm_img_only_file_name():
-    global img_only_file_name
-    img_only_file_name = img_path.get().split(os.path.sep)[-1].replace('.jpg', '').replace('.png', '').replace('.gif', '')
-reconfirm_img_only_file_name()
+
+def get_img_only_file_name(img_path):
+    img_only_file_name = img_path.split(os.path.sep)[-1].replace('.jpg', '').replace('.png', '').replace('.gif', '')
+    return img_only_file_name
 
 def browse_img_path_command():
-    global img_path
-    img_path.set(fd.askopenfilename(parent=window, title='select image file', filetypes=file_types))
-    print(img_path.get())
+    global img_path, img_path_entry_stringvar
+    img_path = fd.askopenfilenames(parent=window, title='select image file', filetypes=file_types)
+    img_path_entry_stringvar_value = ''
+    for i in img_path:
+        img_path_entry_stringvar_value += i + '; '
+    img_path_entry_stringvar.set(img_path_entry_stringvar_value)
+    print(img_path)
+    print(img_path_entry_stringvar.get())
 browse_img_path = ttk.Button(window, text='Browse', command=lambda: browse_img_path_command())
 browse_img_path.grid(column=1, row=1)
 
-def convert_file_button_command(img_path):
-    reconfirm_img_only_file_name()
-    image = Image.open(img_path)
+def convert_file_button_command(img_paths):
+    for img_path in img_paths:
+        img_only_file_name = get_img_only_file_name(img_path)
+        image = Image.open(img_path)
+        save_pdf_command(img_path)
+convert_file_button = ttk.Button(window, text='convert file', command=lambda: convert_file_button_command(img_path))
+convert_file_button.grid(column=1, row=2)
+
+def save_pdf_command(img_path, img_only_file_name=None):
+    if type(img_path) in (tuple, list):
+        for i in img_path:
+            save_pdf_command(i)
+    if img_only_file_name is None: img_only_file_name = get_img_only_file_name(img_path)
     pdf_path = fd.asksaveasfilename(filetypes=file_types, defaultextension=img_only_file_name+'pdf')
     if pdf_path:
         try:
-            with open(img_path.get(), 'wb') as pdf_file:
+            with open(img_path, 'wb') as pdf_file:
                 pdf_bytes = img2pdf.convert(image.filename)
                 file.write(pdf_bytes)
                 image.close()
             print('Successfully made pdf file')
         except Exception as e:
             result = msgbox.askretrycancel('Error Saving File!', 'Error Saving File! \nPlease Retry.')
-            if result: window.after(100, convert_file_button_command, img_path.get())
-convert_file_button = ttk.Button(window, text='convert file', command=lambda: convert_file_button_command(img_path))
-convert_file_button.grid(column=0, row=2)
-
-def save_pdf_command():
-    reconfirm_img_only_file_name()
-    fd.asksaveasfile(filetypes=file_types, defaultextension=img_only_file_name+'.pdf')
-save_pdf = ttk.Button(window, text='save PDF file', command=lambda: save_pdf_command())
-save_pdf.grid(column=1, row=2)
+            if result: window.after(100, convert_file_button_command, img_path, img_only_file_name)(filetypes=file_types, defaultextension=img_only_file_name+'.pdf')
+save_pdf = ttk.Button(window, text='save PDF file', command=lambda: save_pdf_command(img_path_entry_stringvar.split('; ')))
+save_pdf.grid(column=1, row=3)
 
 window.mainloop()
