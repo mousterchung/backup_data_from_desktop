c96bc4b6fae9c725a085347a7b52a7cb988f8e15
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2024-09-14 14:47:57 +0800

    Update img2pdf.py

--- python/img2pdf.py
+++ python/img2pdf.py
@@ -6,13 +6,36 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import fd
 
-root = tk.Tk()
-file_types = (('jpeg files', '*.jpg'), ('png files', '*.png'), ('gif files', '*.gif'), ('all files', '*.*'))
-'''
-file_path = fd.askopenfilename(parent=root, title='select file', filetypes=file_types)
-print(file_path)
-fd.asksaveasfile(filetypes = file_types, defaultextension = 'default file name')
-'''
+
+file_types = (('jpeg files', '*.jpg'), ('png files', '*.png'), ('gif files', '*.gif'), ('pdf files', '*.pdf'), ('all files', '*.*'))
+
+window = tk.Tk()
+window.title = 'image to PDF file converter'
+window.geometry('400x400')
+window.resizable(False, False)
+window.iconbitmap(os.path.join('.', 'img', 'favicon.ico'))
+
+title_label = ttk.Label(text='image to PDF file converter')
+title_label.grid(column=0, row=0, rowspan=2)
+
+img_path = tk.StringVar(window, '')
+img_path_entry = ttk.Entry(window, textvariable=file_path)
+img_path_entry.grid(column=0, row=1)
+def reconfirm_img_only_file_name():
+    global img_only_file_name
+    img_only_file_name = img_path.split(os.path.sep)[-1].replace('.jpg', '').replace('.png', '').replace('.gif', '')
+reconfirm_img_only_file_name()
+
+def browse_file_path_command():
+    global img_path
+    img_path.set(fd.askopenfilename(parent=window, title='select file', filetypes=file_types))
+    print(file_path)
+browse_img_path = ttk.Button(window, text='Browse', command=lambda: browse_file_path_command())
+browse_img_path.grid(column=1, row=1)
+
+def save_pdf_command():
+    reconfirm_img_only_file_name()
+    fd.asksaveasfile(filetypes=file_types, defaultextension=img_only_file_name+'.pdf')
 
 '''
 img_path = 'C:/Users/Admin/Desktop/GfG_images/do_nawab.png' # storing image path
@@ -20,7 +43,9 @@ pdf_path = 'C:/Users/Admin/Desktop/GfG_images/file.pdf' # storing pdf path
 image = Image.open(img_path) # opening image
 pdf_bytes = img2pdf.convert(image.filename) # converting into chunks using img2pdf
 '''
+
 '''only_file_name = image.filename.split(os.path.sep)[-1]'''
+
 '''
 file = open(pdf_path, 'wb') # opening or creating pdf file
 file.write(pdf_bytes) # writing pdf files with chunks
@@ -28,3 +53,5 @@ image.close() # closing image file
 file.close() # closing pdf file
 print('Successfully made pdf file') # output
 '''
+
+window.mainloop()
