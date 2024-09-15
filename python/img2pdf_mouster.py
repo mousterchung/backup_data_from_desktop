9200f08868213b875f4358a8dfed3d1e49d827e1
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2024-09-15 11:27:48 +0800

    Update img2pdf_mouster.py

--- python/img2pdf_mouster.py
+++ python/img2pdf_mouster.py
@@ -15,12 +15,12 @@ pdf_file_types = (('PDF files', '*.pdf'), ('image files', ('*.jpg', '*.jpeg', '*
 img_path = ()
 
 window = tk.Tk()
-window.title = 'image to PDF file converter'
+window.title('Image to PDF File Converter')
 window.geometry('570x200')
 window.resizable(False, False)
 window.iconbitmap(os.path.join('.', 'img', 'favicon.ico'))
 
-title_label = ttk.Label(text='image to PDF file converter')
+title_label = ttk.Label(text='Image to PDF File Converter')
 title_label.grid(column=0, row=0, columnspan=2)
 
 img_path_entry_frame = ttk.Frame(window)
@@ -36,12 +36,12 @@ img_path_entry_scrollbar_horizontal = ttk.Scrollbar(img_path_entry_frame, comman
 img_path_entry_scrollbar_horizontal.grid(column=0, row=1, sticky=tk.W+tk.E)
 
 def get_img_only_file_name(img_path):
-    img_only_file_name = img_path.split(os.path.sep)[-1].replace('.jpg', '').replace('.png', '').replace('.gif', '')
+    img_only_file_name = replace_extension(img_path.split(os.path.sep)[-1])
     return img_only_file_name
 
 def browse_img_path_command():
     global img_path
-    img_path = fd.askopenfilenames(parent=window, title='select image file', filetypes=img_file_types)
+    img_path = fd.askopenfilenames(parent=window, title='Select Image File', filetypes=img_file_types)
     img_path_entry.delete(0, tk.END)
     img_path_entry.insert(tk.END, *img_path)
     print(img_path)
@@ -53,7 +53,7 @@ def convert_file_button_command(img_paths):
         img_only_file_name = get_img_only_file_name(img_path)
         image = Image.open(img_path)
         save_pdf_command(img_path, image=image)
-convert_file_button = ttk.Button(window, text='convert file', command=lambda: convert_file_button_command(img_path))
+convert_file_button = ttk.Button(window, text='Convert to PDF File', command=lambda: convert_file_button_command(img_path))
 convert_file_button.grid(column=1, row=2)
 
 def save_pdf_command(img_path, img_only_file_name=None, image=None):
@@ -61,18 +61,19 @@ def save_pdf_command(img_path, img_only_file_name=None, image=None):
         for i in img_path:
             save_pdf_command(i)
     if img_only_file_name is None: img_only_file_name = get_img_only_file_name(img_path)
-    pdf_path = fd.asksaveasfilename(filetypes=pdf_file_types, defaultextension=img_only_file_name+'.pdf')
+    pdf_path = fd.askdirectory(title=f'Save PDF File `{img_path.split("/")[-1]}`')
     if pdf_path:
         try:
             with open(replace_extension(img_path)+'.pdf', 'wb') as pdf_file:
                 pdf_bytes = img2pdf.convert(image.filename)
                 image.close()
                 pdf_file.write(pdf_bytes)
-            print('Successfully made pdf file')
+            print(f'Successfully Made PDF File. \n`{img_path.split(os.path.sep)[-1]}`')
+            msgbox.showinfo('Successfully Made PDF File.', f'Successfully Made PDF File. \n`{img_path.split("/")[-1]}`')
         except IOError as e:
             result = msgbox.askretrycancel('Error Saving File!', 'Error Saving File! \nPlease Retry.')
             if result: window.after(100, save_pdf_command, img_path, img_only_file_name, image)
-save_pdf = ttk.Button(window, text='save PDF file', command=lambda: save_pdf_command(img_path))
+save_pdf = ttk.Button(window, text='Save PDF File', command=lambda: save_pdf_command(img_path))
 save_pdf.grid(column=1, row=3)
 
 print(img_path)
