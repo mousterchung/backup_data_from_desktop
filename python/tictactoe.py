44d7444a54c1d6a82c23a632dc705c205ebeeff8
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2024-10-04 18:42:53 +0800

    Create tictactoe.py

---
+++ python/tictactoe.py
@@ -0,0 +1,7 @@
+# tictactoe.py, A non-OOP tic-tac-toe game.
+
+ALL_SPACES = list('123456789')  # The keys for a TTT board dictionary.
+X, O, BLANK = 'X', 'O', ' '  # Constants for string values.
+
+def main():
+    
